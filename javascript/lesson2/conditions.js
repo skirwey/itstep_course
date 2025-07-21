054e2e53af8f215cbfffb3a66fab764dba8ed933
группы стационара <st@itstep.lan>
2025-07-21 21:49:36 +0300

    операторы

---
+++ javascript/lesson2/conditions.js
@@ -0,0 +1,133 @@
+/*let n = +prompt('Введите число');
+let m = +prompt('Введите значение степени');
+
+
+
+
+if(n==0) {
+    alert('Результат операций равен 0');
+}
+
+if(n==1) {
+    alert('Результат операций равен 1');
+}
+
+if(n>1) {
+    let result = n**m;
+    alert(`Результат операций равен ${result};`)
+}
+
+
+
+
+if(n >= 0) {
+    let result = n**m;
+    alert(`Результат операций равен ${result}`);
+}
+else {
+    alert('Вы ввели отрицательное число');
+}
+
+
+
+
+if(n==0) {
+    alert('Результат операций равен 0');
+}
+else if(n==1) {
+    alert('Результат операций равен 1');
+}
+else {
+    let result = n**m;
+    alert(`Результат операций равен ${result}`);
+}
+
+
+
+let a = null;
+let b = !a;
+alert(a >= 0);
+console.log(b);
+
+let a = true;
+let b = false;
+alert(!a);
+
+if(n >= 5 && n <= 10 && n != 4) {
+    if(m <= 2 || m >= 4) {
+        let result = n**m;
+        alert(`Результат операций равен ${result}`);
+    }
+}
+
+
+
+
+let nameJavaScript;
+let a = ('Верно!');
+let b = ('Не знаете? ECMAScript!');
+nameJavaScript = prompt('Какое «официальное» название JavaScript?');
+
+if(nameJavaScript == 'ECMAScript') {
+    console.log(a);
+}
+else {
+    console.log(b);
+}
+
+
+
+let number;
+number = prompt('Введите число');
+
+if(number==0) {
+    alert('Результат равен 0');
+}
+else if(number>0) {
+    alert('Результат равен 1');
+}
+else {
+    alert('Результат равен -1');
+}
+
+
+
+
+let age = 110;
+ if(age <= 0 || age > 120) {
+     console.log('Возраст неверный!')
+ }
+
+
+
+
+let hours = 34;
+let minutes = 43;
+let seconds = 56;
+
+if((hours < 0 || hours > 24) || (minutes < 0 || minutes > 60) || (seconds < 0 || seconds > 60)) {
+    console.log('Данные введены неверно');
+}*/
+
+
+
+let num1 = +prompt('Введите первое число');
+let num2 = +prompt('Введите второе число');
+let znak = prompt('Введите знак');
+
+switch(znak) {
+    case '+':
+        console.log(num1 + num2);
+    break;
+    case '-':
+        console.log(num1 - num2);
+    break;
+    case '*':
+        console.log(num1 * num2);
+    break;
+    case '/':
+        console.log(num1 / num2);
+    break;
+}
+
+
