35a721f7361fc7bd12f79c09cb2a38df2ba311c7
retret664 <tretakaryj@gmail.com>
2025-10-08 21:48:19 +0300

    сайт2

---
+++ html/maket7/maket7.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    width: 1110,
+    height: 555,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
