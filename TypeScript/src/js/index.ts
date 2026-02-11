58bd654d730766fc0d8ffab3b81ac2dec3b08489
retret664 <kodinetsdaniil@gmail.com>
2026-02-11 21:42:42 +0300

    typescript

---
+++ TypeScript/src/js/index.ts
@@ -0,0 +1,29 @@
+import '../scss/index.scss';
+
+let n: number = 5;
+
+const city: string = "Питер";
+
+let result: boolean = true;
+
+let m: number | undefined;
+
+let some: any;
+
+let color: "red" | "green" | "blue";
+
+let status: 200 | 404 | 500;
+
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean
+};
+
+let user: Person = {
+    name: "Иван Иванович",
+    age: 35,
+    isStudent: false
+}
+
+
